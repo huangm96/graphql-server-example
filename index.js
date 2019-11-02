33aa7067bd17c88b050606e02575f50c0df60ecb
Min-Huang <minh.huang96@gmail.com>
2019-11-01 21:44:15 -0700

    added resolvers

--- index.js
+++ index.js
@@ -19,3 +19,30 @@ const typeDefs = gql`
     books: [Book]
   }
 `;
+
+const books = [
+  {
+    title: "Harry Potter and the Chamber of Secrets",
+    author: "J.K. Rowling"
+  },
+  {
+    title: "Jurassic Park",
+    author: "Michael Crichton"
+  }
+];
+
+const resolvers = {
+  Query: {
+    books: () => books
+  }
+};
+
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
